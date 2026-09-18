bfdf1e8fc2b14341f7a63e56d5acbaed225053f6
Gavin Stephens <gavin.stephens@freelancercolo.local>
2026-09-18 20:43:02 +0100

    Configure SimplyGeek custom domain and enable search indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import optimizeImages from "./integrations/optimize-images.mjs";
 
 export default defineConfig({
-  site: "https://simplygeekuk.github.io",
+  site: "https://simplygeek.co.uk",
   output: "static",
   trailingSlash: "always",
   integrations: [optimizeImages()],
